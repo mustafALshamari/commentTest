667a557a13372f52c30e26ce7577af9ed03820d1
Mustafa <mustafa.gentleman2011@gmail.com>
2019-04-02 16:15:12 +0400

    for Herokuaaaadsada

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,11 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::get('/','CommentController@list')->name('welcome');
 Route::get('/commentList','CommentController@list');
 Route::post('/addComment', 'CommentController@addComment');
